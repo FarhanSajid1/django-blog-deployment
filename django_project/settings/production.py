a7841ba57c896e38be5ac621bf5830a6311fcb55
Farhan Sajid <FarhanSajid7861@gmail.com>
2019-03-29 15:16:36 -0400

    deployment environment

--- django_project/settings/production.py
+++ django_project/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = False
+DEBUG = True
 
 SECRET_KEY =  'afoiajsfoija84284uijskflkamlfkmaskfjaofjoa'
 
@@ -16,26 +16,13 @@ DATABASES = {
 }
 
 ALLOWED_HOSTS = [
-    '127.0.0.1:8990',
-    '127.0.0.1'
+    '*'
 ]
-'''Adding s3 Support'''
-INSTALLED_APPS +=['storages']
-
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-AWS_S3_CUSTOM_DOMAIN = f'http://s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}'
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# static/media urls and roots
+MEDIA_ROOT = '/var/www/blog/media/'
+STATIC_ROOT = '/var/www/blog/static/'
+STATIC_URL = '/static/'
 
 
-# S3 CONFIGURATIONS
-AWS_STATIC_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_STATIC_LOCATION}/'
 
-AWS_MEDIA_LOCATION = 'media'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_MEDIA_LOCATION}/'
+
---
+++ testing.py
@@ -0,0 +1,4 @@
+import os
+
+bucket = os.environ.get('AWS_ACCESS_KEY_ID')
+print(os.environ)
